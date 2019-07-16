87f0a9ca20134a6e3111d83cb8978a98e0c628e0
Kaleb Fischer <kalebfischer@flywheel.io>
2019-07-15 19:04:24 -0500

    update manifests and fix options in run script

--- run.py
+++ run.py
@@ -35,6 +35,73 @@ def validate_nifti(nifti_name, nifti_input_path):
     return nifti_path
 
 
+def create_options_list(config, siena_or_sienax):
+    # Initialize options list
+    options_list = list()
+
+    # Append common booleans
+    if config.get('DEBUG'):
+        options_list.append('-d')
+    if config.get('SEG'):
+        options_list.append('-2')
+    if config.get('T2'):
+        options_list.append('-t2')
+
+    # Append common strings
+    bet_options = config.get('BET')
+    if bet_options != '':
+        options_list.append('-B')
+        # BET options must be wrapped in double quotes
+        if not bet_options.startswith('"'):
+            bet_options = '"{}"'.format(bet_options)
+        options_list.append(bet_options)
+    bottom_boundary = config.get('BOTTOM')
+    if bottom_boundary != '':
+        options_list.append('-b')
+        options_list.append(bottom_boundary)
+    top_boundary = config.get('TOP')
+    if top_boundary != '':
+        options_list.append('-t')
+        options_list.append(top_boundary)
+
+    # SIENA-specific options
+    if siena_or_sienax == 'siena':
+        # Add SIENA-specific boolean flags
+        if config.get('SS_BET'):
+            options_list.append('-m')
+        if config.get('VENT'):
+            options_list.append('-V')
+        # Add SIENA-specific string options
+        siena_diff_options = config.get('S_DIFF')
+        if siena_diff_options != '':
+            options_list.append('-S')
+            if not siena_diff_options.startswith('"'):
+                siena_diff_options = '"{}"'.format(siena_diff_options)
+            options_list.append(siena_diff_options)
+
+    # SIENAX-specific options
+    elif siena_or_sienax == 'sienax':
+        # Add SIENAX-specific boolean flags
+        if config.get('REGIONAL'):
+            options_list.append('-r')
+
+        # Add SIENAX-specific string options
+        fast_options_sienax = config.get('S_FAST')
+        if fast_options_sienax != '':
+            options_list.append('-S')
+            # FAST options must be wrapped in double quotes
+            if not fast_options_sienax.startswith('"'):
+                fast_options_sienax = '"{}"'.format(fast_options_sienax)
+            options_list.append(fast_options_sienax)
+    # Handle function misuse
+    else:
+        log.error('Invalid siena_or_sienax string provided: {}'.format(siena_or_sienax))
+        log.error('Algorithm will not run. Exiting...')
+        os.sys.exit(1)
+    # Return options list
+    return options_list
+
+
 if __name__ == '__main__':
     with flywheel.GearContext() as gear_context:
 
@@ -45,119 +112,63 @@ if __name__ == '__main__':
         # Get output filepath
         output_directory = gear_context.output_dir
 
-        # Get inputs from manifest
-        nifti_1 = gear_context.get_input('nifti_1')
-        nifti_2 = gear_context.get_input('nifti_2')
-        ventricle_mask = gear_context.get_input('ventricle_mask')
-        lesion_mask = gear_context.get_input('lesion_mask')
-        # Validate inputs and set file path(s)
-        nifti_1_path = validate_nifti('nifti_1', nifti_1['location']['path'])
-        if nifti_2:
-            nifti_2_path = validate_nifti('nifti_2', nifti_2['location']['path'])
-        if ventricle_mask:
-            ventricle_mask_path = validate_nifti('ventricle_mask', ventricle_mask['location']['path'])
-        if lesion_mask:
-            lesion_mask_path = validate_nifti('lesion_mask', lesion_mask['location']['path'])
-
         # Get config options
         config = gear_context.config
-        bet_options = config.get('BET')
-        bottom_boundary = config.get('BOTTOM')
-        run_debug = config.get('DEBUG')
-        regional_sienax = config.get('REGIONAL')
-        run_siena_with_single_input = config.get('siena_single_input')
-        two_class_segmentation = config.get('SEG')
-        fast_options_sienax = config.get('S_FAST')
-        ss_bet = config.get('SS_BET')
-        siena_diff_options = config.get('S_DIFF')
-        t2_weighted = config.get('T2')
-        top_boundary = config.get('TOP')
-        run_viena = config.get('VENT')
+        log.info('Config type {}'.format(type(config)))
 
-        # Determine if Siena or SienaX
-        if not nifti_2 and not run_siena_with_single_input:
-            log.info('SIENAX configuration provided...')
-            # Initialize command list
-            command_list = list()
-            command_list.append('sienax')
-        elif not nifti_2 and run_siena_with_single_input:
-            log.info('SIENA single-input configuration provided...')
-            nifti_2_path = nifti_1_path
-            # Initialize command list
-            command_list = list()
+        # Initialize command_list
+        command_list = list()
+
+        # Determine if SIENA or SIENAX
+        if gear_context.get_input('NIFTI_1') and gear_context.get_input('NIFTI_2'):
+            log.info('Getting FSL SIENA Configuration...')
+            # Add siena command to command list
             command_list.append('siena')
-        else:
-            log.info('SIENA 2-input configuration provided...')
-            # Initialize command list
-            command_list = list()
-            command_list.append('siena')
-        # Append shared config options (used for both Siena and SienaX)
-        # Append boolean option flags to command
-        if run_debug:
-            command_list.append('-d')
-        if two_class_segmentation:
-            command_list.append('-2')
-        if t2_weighted:
-            command_list.append('-t2')
-        # Append string option flags to command
-        if bet_options != '':
-            command_list.append('-B')
-            # BET options must be wrapped in double quotes
-            if not bet_options.startswith('"'):
-                bet_options = '"{}"'.format(bet_options)
-            command_list.append(bet_options)
-        if bottom_boundary != '':
-            command_list.append('-b')
-            command_list.append(bottom_boundary)
-        if top_boundary != '':
-            command_list.append('-t')
-            command_list.append(top_boundary)
-
-        # Add SIENAX-specific flags to command
-        if command_list[0] == 'sienax':
-
-            # Add file path to command
+            # Get inputs from manifest
+            nifti_1 = gear_context.get_input('NIFTI_1')
+            nifti_2 = gear_context.get_input('NIFTI_2')
+            ventricle_mask = gear_context.get_input('ventricle_mask')
+            # Validate inputs and append to command
+            nifti_1_path = validate_nifti('nifti_1', nifti_1['location']['path'])
             command_list.append(nifti_1_path)
-
-            # Add boolean flags
-            if regional_sienax:
-                command_list.append('-r')
-
-            # Add string flags
-            if fast_options_sienax != '':
-                command_list.append('-S')
-                # FAST options must be wrapped in double quotes
-                if not fast_options_sienax.startswith('"'):
-                    fast_options_sienax = '"{}"'.format(fast_options_sienax)
-                command_list.append(fast_options_sienax)
-
-            # Add lesion mask
-            if lesion_mask:
-                command_list.append('-lm')
-                command_list.append(lesion_mask_path)
-
-        # Add SIENA-specific flags to command
-        elif command_list[0] == 'siena':
-
-            # Add file paths to command
-            command_list.append(nifti_1_path)
+            nifti_2_path = validate_nifti('nifti_2', nifti_2['location']['path'])
             command_list.append(nifti_2_path)
 
-            # Add boolean flags
-            if run_viena:
-                command_list.append('-V')
-
-            # Add ventricle mask
+            # Get options from config
+            command_options = create_options_list(config, 'siena')
+            # Add optional ventricle mask to options
             if ventricle_mask:
-                command_list.append('-v')
-                command_list.append(ventricle_mask_path)
+                ventricle_mask_path = validate_nifti('ventricle_mask', ventricle_mask['location']['path'])
+                command_options.append('-v')
+                command_options.append(ventricle_mask_path)
+            # Add options to command list
+            command_list = command_list + command_options
 
-        # Append output configuration to command
+        elif gear_context.get_input('NIFTI'):
+            log.info('Getting FSL SIENAX Configuration...')
+            # Add sienax command to command list
+            command_list.append('sienax')
+            # Get inputs from manifest
+            nifti = gear_context.get_input('NIFTI')
+            # Validate inputs
+            nifti_path = validate_nifti('nifti', nifti['location']['path'])
+            command_list.append(nifti_path)
+            lesion_mask = gear_context.get_input('lesion_mask')
+            # Get options from config
+            command_options = create_options_list(config, 'sienax')
+            if lesion_mask:
+                lesion_mask_path = validate_nifti('lesion_mask', lesion_mask['location']['path'])
+            # Add options to command list
+            command_list = command_list + command_options
+
+        else:
+            log.error('Invalid manifest.json file provided for FSL SIENA/SIENAX')
+            log.error('Algorithm will not run. Exiting...')
+            os.sys.exit(1)
+
+        # Add output directory to command list
         command_list.append('-o')
-        command_list.append(output_directory)
-
-        # Configure environment
-        log.info('Configuring FSL environment...')
+        command_list.append('output_directory')
 
         # Echo command before running
         echo_command = list()
